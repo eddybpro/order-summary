0faf3d9e78e78eff565a82d1945e635249d73412
eddybpro <abdelilahdb154@gmail.com>
2023-08-24 07:27:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import illustrationHero from "./assets/illustration-hero.svg";
+import MusicIcon from "./assets/icon-music.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="head">
+        <img src={illustrationHero} alt="illustration hero" />
+      </div>
+      <div className="txt-container">
+        <h1 className="title">order summary</h1>
+        <p className="para">
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+        <div className="price-box">
+          <div className="icon-price-box">
+            <img src={MusicIcon} alt="" />
+            <div className="price">
+              <h4>annual plan</h4>
+              <p>$59.99/year</p>
+            </div>
+          </div>
+
+          <a href="#" className="link">
+            change
+          </a>
+        </div>
+
+        <button className="pay">proceed to payment</button>
+        <button className="cancel">cancel order</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
